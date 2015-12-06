7eeb9c6dcddd550eb53c060f9ce8839dd536a2f3
Christopher Abad <aempirei@gmail.com>
2015-12-06 00:10:56 -0800

    .

--- code.js
+++ code.js
@@ -2,45 +2,49 @@ var state;
 
 function initState() {
 
-	state = {
-		active:		false,
-		dirty:		false,
-		n:			0,
-		path:		null,
-		paths:		[],
-		sp:			null,
-		width:		365,
-		height:		300,
-		precision:	4,
-		normal:		256,
-		step:		1,
-		debug:		false,
-		grid:		true,
-		gap:		17
-	};
+		state = {
+				active:		false,
+				dirty:		false,
+				n:			0,
+				path:		null,
+				paths:		[],
+				sp:			null,
+				width:		365,
+				height:		300,
+				precision:	4,
+				normal:		256,
+				step:		1,
+				debug:		false,
+				grid:		true,
+				gap:		17
+		};
 
-	bs.disabled = true;
-	br.disabled = true;
+		bs.disabled = true;
+		br.disabled = true;
 
-	s.style.border = "1px solid black";
+		s.style.border = "1px solid black";
 
-	con.innerHTML = "";
-	dbg.innerHTML = "";
-	s.innerHTML = "";
+		con.innerHTML = "";
+		dbg.innerHTML = "";
+		s.innerHTML = "";
 
-	s.appendChild(createGrid('grid', state.width, state.height, state.gap));
+		s.appendChild(createGrid('grid', state.width, state.height, state.gap));
 
-	bgl.style.backgroundColor = state.grid ? "black" : "white";
-	bdl.style.backgroundColor = state.debug ? "black" : "white";
+		bgl.style.backgroundColor = state.grid ? "black" : "white";
+		bdl.style.backgroundColor = state.debug ? "black" : "white";
 };
 
 var svgns = "http://www.w3.org/2000/svg";
 
+function fpcompare(a,b) {
+		return a.toPrecision(state.precision) == b.toPrecision(state.precision);
+}
+
 function hypot() {
-	var x = 0;
-	for(var n = 0; n < arguments.length; n++)
-		x += Math.pow(arguments[n], 2);
-	return Math.sqrt(x);
+		var x = 0;
+		for(var n = 0; n < arguments.length; n++)
+				x += Math.pow(arguments[n], 2);
+		return Math.sqrt(x);
 }
 
 function evalue() {
@@ -52,13 +56,13 @@ function dot(u,v) {
 		var x = 0;
 		for(var i = 0; i < u.length; i++)
 				x += u[i] * v[i];
-		
+
 }
 
 function Path(dim) {
-	this.vector = [];
-	for(var n = 0; n < dim; n++)
-		this.vector.push([]);
+		this.vector = [];
+		for(var n = 0; n < dim; n++)
+				this.vector.push([]);
 }
 
 Path.mean = function() {
@@ -108,74 +112,74 @@ Path.covariance = function() {
 
 Path.eigensystem = function() {
 
-	var sigma = Path.covariance.apply(null, arguments);
+		var sigma = Path.covariance.apply(null, arguments);
 
-	var lambda = new Path(arguments[0].dimensions());
+		var lambda = new Path(arguments[0].dimensions());
 
-	switch(lambda.dimensions()) {
+		switch(lambda.dimensions()) {
 
-			case 1:
+				case 1:
 
-				for(var n = 0; n < sigma.length(); n++)
-						lambda.vector[0][n] = 1;
+						for(var n = 0; n < sigma.length(); n++)
+								lambda.vector[0][n] = 1;
 
-				break;
+						break;
 
-			case 2:
+				case 2:
 
-				for(var n = 0; n < sigma.length(); n++) {
+						for(var n = 0; n < sigma.length(); n++) {
 
-						var a = sigma.vector[0][n];
-						var b = sigma.vector[1][n];
-						var c = sigma.vector[2][n];
-						var d = sigma.vector[3][n];
+								var a = sigma.vector[0][n];
+								var b = sigma.vector[1][n];
+								var c = sigma.vector[2][n];
+								var d = sigma.vector[3][n];
 
-						var detA = a * d - b * c;
+								var detA = a * d - b * c;
 
-						var s = (a + d) / 2;
+								var s = (a + d) / 2;
 
-						var t = Math.sqrt((s * s) - detA);
+								var t = Math.sqrt((s * s) - detA);
 
-						var X1 = s + t;
-						var X2 = s - t;
+								var X1 = s + t;
+								var X2 = s - t;
 
-						lambda.vector[0][n] = X1;
-						lambda.vector[1][n] = X2;
+								lambda.vector[0][n] = X1;
+								lambda.vector[1][n] = X2;
 
-						var x1 = X1 * (d - b) - detA;
-						var y1 = X1 * (a - c) - detA;
+								var x1 = X1 * (d - b) - detA;
+								var y1 = X1 * (a - c) - detA;
 
-						var h1 = hypot(x1, y1);
+								var h1 = hypot(x1, y1);
 
-						var x2 = X2 * (d - b) - detA;
-						var y2 = X2 * (a - c) - detA;
+								var x2 = X2 * (d - b) - detA;
+								var y2 = X2 * (a - c) - detA;
 
-						var h2 = hypot(x2, y2);
+								var h2 = hypot(x2, y2);
 
-						sigma.vector[0][n] = x1 / h1;
-						sigma.vector[1][n] = y1 / h1;
-						sigma.vector[2][n] = x2 / h2;
-						sigma.vector[3][n] = y2 / h2;
-				}
+								sigma.vector[0][n] = x1 / h1;
+								sigma.vector[1][n] = y1 / h1;
+								sigma.vector[2][n] = x2 / h2;
+								sigma.vector[3][n] = y2 / h2;
+						}
 
-				break;
+						break;
 
-			default:
+				default:
 
-				window.alert("dimensionality of " + lambda.dimensions() + " too high.");
+						window.alert("dimensionality of " + lambda.dimensions() + " too high.");
 
-				for(var n = 0; n < sigma.length(); n++)
-						for(var m = 0; m < lambda.dimensions(); m++)
-								lambda.vector[m][n] = 0;
+						for(var n = 0; n < sigma.length(); n++)
+								for(var m = 0; m < lambda.dimensions(); m++)
+										lambda.vector[m][n] = 0;
 
-				break;
-	}
+						break;
+		}
 
-	return [ lambda, sigma ];
+		return [ lambda, sigma ];
 };
 
 Path.prototype.dimensions = function() {
-	return this.vector.length;
+		return this.vector.length;
 };
 
 Path.prototype.toString = function(type) {
@@ -220,100 +224,103 @@ Path.prototype.toString = function(type) {
 };
 
 Path.prototype.normalize = function(norm) {
-	var coef = norm / this.arcLength();
-	for(var n = 0; n < this.length(); n++)
-		for(m = 0; m < this.dimensions(); m++)
-			this.vector[m][n] *= coef;
+		var coef = norm / this.arcLength();
+		for(var n = 0; n < this.length(); n++)
+				for(m = 0; m < this.dimensions(); m++)
+						this.vector[m][n] *= coef;
 };
 
 Path.prototype.arcLength = function() {
-	var x = 0;
-	var args = [];
-	for(var n = 0; n < this.length(); n++) {
-		for(var m = 0; m < this.dimensions(); m++)
-			args[m] = this.vector[m][n];
-		x += hypot.apply(null, args);
-	}
-	return x;
+		var x = 0;
+		var args = [];
+		for(var n = 0; n < this.length(); n++) {
+				for(var m = 0; m < this.dimensions(); m++)
+						args[m] = this.vector[m][n];
+				x += hypot.apply(null, args);
+		}
+		return x;
 };
 
 Path.prototype.length = function() {
-	return this.vector[0].length;
+		return this.vector[0].length;
+};
+
+Path.prototype.unshift = function() {
+		for(var m = 0; m < this.dimensions(); m++)
+				this.vector[m].unshift(arguments[m]);
 };
 
 Path.prototype.push = function() {
-	for(var m = 0; m < this.dimensions(); m++)
-		this.vector[m].push(arguments[m]);
+		for(var m = 0; m < this.dimensions(); m++)
+				this.vector[m].push(arguments[m]);
 };
 
 Path.prototype.dx = function() {
-
-	for(var n = 1; n < this.length(); n++)
-		for(var m = 0; m < this.dimensions(); m++)
-			this.vector[m][n - 1] = this.vector[m][n] - this.vector[m][n - 1];
-
-	for(var m = 0; m < this.dimensions(); m++)
-		this.vector[m].pop();
+		for(var m = 0; m < this.dimensions(); m++) {
+				for(var n = 1; n < this.length(); n++)
+						this.vector[m][n - 1] = this.vector[m][n] - this.vector[m][n - 1];
+				this.vector[m].pop();
+		}
 };
 
 Path.prototype.column = function(n) {
-	var v = [];
-	for(var m = 0; m < this.dimensions(); m++)
-		v.push(this.vector[m][n]);
-	return v;
+		var v = [];
+		for(var m = 0; m < this.dimensions(); m++)
+				v.push(this.vector[m][n]);
+		return v;
 };
 
 Path.prototype.zero = function() {
-	var v = [];
-	for(var m = 0; m < this.dimensions(); m++)
-		v.push(0);
-	return v;
+		var v = [];
+		for(var m = 0; m < this.dimensions(); m++)
+				v.push(0);
+		return v;
 }
 
 Path.prototype.step = function(n,x,r) {
 
-	if(n == this.length())
-		return { n: n, x: 0, v: this.zero() };
+		if(n == this.length())
+				return { n: n, x: 0, v: this.zero() };
 
-	var v = this.column(n);
-	var h = hypot.apply(null, v);
-	var dh = (1 - x) * h;
+		var v = this.column(n);
+		var h = hypot.apply(null, v);
+		var dh = (1 - x) * h;
 
-	if(r < dh) {
+		if(r < dh) {
 
-		var coef = r / h;
+				var coef = r / h;
+
+				for(var i = 0; i < v.length; i++)
+						v[i] *= coef;
+
+				return { n: n, x: x + coef, v: v };
+
+		}
+
+		var w = this.step(n + 1, 0, r - dh);
 
 		for(var i = 0; i < v.length; i++)
-			v[i] *= coef;
+				w.v[i] += (1 - x) * v[i];
 
-		return { n: n, x: x + coef, v: v };
-
-	}
-
-	var w = this.step(n + 1, 0, r - dh);
-
-	for(var i = 0; i < v.length; i++)
-		w.v[i] += (1 - x) * v[i];
-
-	return w;
+		return w;
 };
 
 Path.prototype.resample = function(step) {
 
-	var path = new Path(this.dimensions());
-	var n = 0;
-	var x = 0;
+		var path = new Path(this.dimensions());
+		var n = 0;
+		var x = 0;
 
-	var samples = Math.round(this.arcLength() / step);
+		var samples = Math.round(this.arcLength() / step);
 
-	while(n < this.length() && path.length() < samples) {
-		var w = this.step(n, x, step);
-		n = w.n;
-		x = w.x;
-		this.push.apply(path, w.v);
-	}
+		while(n < this.length() && path.length() < samples) {
+				var w = this.step(n, x, step);
+				n = w.n;
+				x = w.x;
+				this.push.apply(path, w.v);
+		}
 
-	return path;
+		return path;
 };
 
 Path.prototype.range = function() {
@@ -359,7 +366,7 @@ Path.prototype.createElement = function() {
 };
 
 function alignment(p, q) {
-			
+
 		var gap = -1;
 
 		var W = p.length() + 1;
@@ -394,89 +401,130 @@ function alignment(p, q) {
 						var Match = F(i-1, j-1) + S(i, j);
 						var Delete = F(i-1, j) + gap;
 						var Insert = F(i, j-1) + gap;
+
 						m[i + j * W] = Math.max(Match, Insert, Delete);
 				}
 		}
+
+		var A = new Path(p.dimensions());
+		var B = new Path(q.dimensions());
+
+		var i = p.length();
+		var j = q.length();
+
+		while(i > 0 || j > 0) {
+
+				var Ai;
+				var Bj;
+
+				if (i > 0 && j > 0 && fpcompare(F(i,j), F(i-1, j-1) + S(i, j))) {
+
+						Ai = p.column(i - 1);
+						Bj = q.column(j - 1);
+
+						i--;
+						j--;
+
+				} else if (i > 0 && fpcompare(F(i,j), F(i-1,j) + gap)) {
+
+						Ai = p.column(i - 1);
+						Bj = q.zero();
+
+						i--;
+
+				} else {
+
+						Ai = p.zero();
+						Bj = q.column(j - 1);
+
+						j--;
+				}
+
+				A.unshift(Ai);
+				B.unshift(Bj);
+		}
+
+		return [ A, B ];
 }
 
 function log(o,str) {
-	if(state.debug) {
-		state.n++;
-		o.innerHTML += "[" + state.n.toString() + "] " + str + "<br>";
-		o.scrollTop = o.scrollHeight;
-	}
+		if(state.debug) {
+				state.n++;
+				o.innerHTML += "[" + state.n.toString() + "] " + str + "<br>";
+				o.scrollTop = o.scrollHeight;
+		}
 }
 
 function toString(n) {
-	return n.toPrecision(state.precision);
+		return n.toPrecision(state.precision);
 }
 
 function setDim(o,x,y) {
-	o.style.width = x.toString() + "px";
-	o.style.height = y.toString() + "px";
+		o.style.width = x.toString() + "px";
+		o.style.height = y.toString() + "px";
 }
 
 function createGrid(id, width, height, gap) {
 
-	var e = document.createElementNS(svgns, "path");
+		var e = document.createElementNS(svgns, "path");
 
-	e.setAttribute("id", id);
+		e.setAttribute("id", id);
 
-	e.setAttribute("stroke","lightsteelblue");
-	e.setAttribute("stroke-width","1px");
-	e.setAttribute("fill", "none");
+		e.setAttribute("stroke","lightsteelblue");
+		e.setAttribute("stroke-width","1px");
+		e.setAttribute("fill", "none");
 
-	var path = "";
+		var path = "";
 
-	for(var y = gap - 0.5; y < height; y += gap)
-		path += "M 0 " + y.toString() + " l " + width.toString() + " 0 ";
+		for(var y = gap - 0.5; y < height; y += gap)
+				path += "M 0 " + y.toString() + " l " + width.toString() + " 0 ";
 
-	for(var x = gap - 0.5; x < width; x += gap)
-		path += "M " + x.toString() + " 0 l 0 " + height.toString() + " ";
+		for(var x = gap - 0.5; x < width; x += gap)
+				path += "M " + x.toString() + " 0 l 0 " + height.toString() + " ";
 
-	e.setAttribute("d", path);
+		e.setAttribute("d", path);
 
-	return e;
+		return e;
 }
 
 function pen_down(e) {
 
-	log(dbg, "svg pen_down(" + e.type + ")");
+		log(dbg, "svg pen_down(" + e.type + ")");
 
-	if(!state.active) {
+		if(!state.active) {
 
-		state.active = true;
+				state.active = true;
 
-		s.style.border = "1px dashed black";
+				s.style.border = "1px dashed black";
 
-		if(!state.dirty) {
+				if(!state.dirty) {
 
-			state.dirty = true;
+						state.dirty = true;
 
-			br.disabled = false;
-			bs.disabled = false;
+						br.disabled = false;
+						bs.disabled = false;
 
-			log(dbg, "buttons enabled!");
+						log(dbg, "buttons enabled!");
+				}
+
+				if(e.type == "touchstart") {
+						e.offsetX = e.touches[0].pageX;
+						e.offsetY = e.touches[0].pageY;
+				}
+
+				state.path = new Path(2);
+
+				state.path.push(e.offsetX, e.offsetY);
+
+				state.sp = document.createElementNS(svgns, "path");
+
+				state.sp.setAttribute("stroke","green");
+				state.sp.setAttribute("stroke-width","2px");
+				state.sp.setAttribute("fill", "none");
+				state.sp.setAttribute("d", "M" + e.offsetX.toString() + " " + e.offsetY.toString());
+
+				s.appendChild(state.sp);
 		}
-
-		if(e.type == "touchstart") {
-			e.offsetX = e.touches[0].pageX;
-			e.offsetY = e.touches[0].pageY;
-		}
-
-		state.path = new Path(2);
-
-		state.path.push(e.offsetX, e.offsetY);
-
-		state.sp = document.createElementNS(svgns, "path");
-
-		state.sp.setAttribute("stroke","green");
-		state.sp.setAttribute("stroke-width","2px");
-		state.sp.setAttribute("fill", "none");
-		state.sp.setAttribute("d", "M" + e.offsetX.toString() + " " + e.offsetY.toString());
-
-		s.appendChild(state.sp);
-	}
 }
 
 function createGlyph(path) {
@@ -501,116 +549,116 @@ function createGlyph(path) {
 
 function pen_up(e) {
 
-	log(dbg, "svg pen_up(" + e.type + ")");
+		log(dbg, "svg pen_up(" + e.type + ")");
 
-	if(state.active) {
+		if(state.active) {
 
-		state.active = false;
+				state.active = false;
 
-		s.style.border = "1px solid black";
+				s.style.border = "1px solid black";
 
-		state.path.dx();
-		state.path.normalize(state.normal);
-		state.path = state.path.resample(state.step);
+				state.path.dx();
+				state.path.normalize(state.normal);
+				state.path = state.path.resample(state.step);
 
-		state.paths.push(state.path);
+				state.paths.push(state.path);
 
-		s.removeChild(state.sp);
+				s.removeChild(state.sp);
 
-		var svg = createGlyph(state.path);
+				var svg = createGlyph(state.path);
 
-		var path_element = state.path.createElement();
+				var path_element = state.path.createElement();
 
-		path_element.setAttribute("stroke","red");
+				path_element.setAttribute("stroke","red");
 
-		svg.appendChild(path_element);
+				svg.appendChild(path_element);
 
-		con.appendChild(svg);
+				con.appendChild(svg);
 
-		log(dbg, "paths=" + state.paths.length.toString() + " path=" + state.path.length().toString());
-	}
+				log(dbg, "paths=" + state.paths.length.toString() + " path=" + state.path.length().toString());
+		}
 }
 
 function pen_move(e) {
 
-	if(state.active) {
+		if(state.active) {
 
-		if(e.type == "touchmove") { 
-			e.offsetX = e.touches[0].pageX;
-			e.offsetY = e.touches[0].pageY;
+				if(e.type == "touchmove") { 
+						e.offsetX = e.touches[0].pageX;
+						e.offsetY = e.touches[0].pageY;
+				}
+
+				state.path.push(e.offsetX, e.offsetY);
+
+				log(dbg, "svg pen_move(" + e.type + "): #" + state.path.length() + " <" + toString(e.offsetX) + "," + toString(e.offsetY) + ">");
+
+				state.sp.setAttribute("d", state.sp.getAttribute("d") + " L" + e.offsetX.toString() + " " + e.offsetY.toString());
 		}
-
-		state.path.push(e.offsetX, e.offsetY);
-
-		log(dbg, "svg pen_move(" + e.type + "): #" + state.path.length() + " <" + toString(e.offsetX) + "," + toString(e.offsetY) + ">");
-
-		state.sp.setAttribute("d", state.sp.getAttribute("d") + " L" + e.offsetX.toString() + " " + e.offsetY.toString());
-	}
 }
 
 window.onload = function(e) {
 
-	initState();
-
-	s.setAttribute("width", state.width);
-	s.setAttribute("height", state.height);
-
-	setDim(dbg, state.width, state.height);
-	setDim(con, dbg.offsetWidth + dash.offsetWidth + s.offsetWidth + 12, state.height);
-
-	log(dbg, "Hello there, mother fucker!");
-
-	s.onmouseleave = pen_up;
-	s.onmouseup = pen_up;
-	s.onmousedown = pen_down;
-
-	s.ontouchstart = pen_down;
-	s.ontouchend = pen_up;
-
-	s.onmousemove = pen_move;
-	s.ontouchmove = pen_move;
-
-	bs.onclick = function(e) {
-
-		log(dbg, "save button clicked!");
-
-		// aggregate paths
-
-		// compute mean path
-		// compute path covariance
-		// compute path harmonics
-		// compute path signature
-
-		var mean_path = Path.mean.apply(null, state.paths);
-
-		var svg = createGlyph(mean_path);
-
-		var path_element = mean_path.createElement();
-
-		path_element.setAttribute("stroke","gray");
-
-		svg.appendChild(path_element);
-
-		con.appendChild(svg);
-
-		var eigen = Path.eigensystem.apply(null, state.paths);
-
-		// initState();
-	};
-
-	br.onclick = function(e) {
 		initState();
-		log(dbg, "reset button clicked!");
-	};
 
-	bd.onclick = function(e) {
-		state.debug = !state.debug;
-		bdl.style.backgroundColor = state.debug ? "black" : "white";
-	}
+		s.setAttribute("width", state.width);
+		s.setAttribute("height", state.height);
 
-	bg.onclick = function(e) {
-		state.grid = !state.grid;
-		bgl.style.backgroundColor = state.grid ? "black" : "white";
-		document.getElementById("grid").setAttribute("stroke", state.grid ? "lightsteelblue" : "white");
-	}
+		setDim(dbg, state.width, state.height);
+		setDim(con, dbg.offsetWidth + dash.offsetWidth + s.offsetWidth + 12, state.height);
+
+		log(dbg, "Hello there, mother fucker!");
+
+		s.onmouseleave = pen_up;
+		s.onmouseup = pen_up;
+		s.onmousedown = pen_down;
+
+		s.ontouchstart = pen_down;
+		s.ontouchend = pen_up;
+
+		s.onmousemove = pen_move;
+		s.ontouchmove = pen_move;
+
+		bs.onclick = function(e) {
+
+				log(dbg, "save button clicked!");
+
+				// aggregate paths
+
+				// compute mean path
+				// compute path covariance
+				// compute path harmonics
+				// compute path signature
+
+				var mean_path = Path.mean.apply(null, state.paths);
+
+				var svg = createGlyph(mean_path);
+
+				var path_element = mean_path.createElement();
+
+				path_element.setAttribute("stroke","gray");
+
+				svg.appendChild(path_element);
+
+				con.appendChild(svg);
+
+				var eigen = Path.eigensystem.apply(null, state.paths);
+
+				// initState();
+		};
+
+		br.onclick = function(e) {
+				initState();
+				log(dbg, "reset button clicked!");
+		};
+
+		bd.onclick = function(e) {
+				state.debug = !state.debug;
+				bdl.style.backgroundColor = state.debug ? "black" : "white";
+		}
+
+		bg.onclick = function(e) {
+				state.grid = !state.grid;
+				bgl.style.backgroundColor = state.grid ? "black" : "white";
+				document.getElementById("grid").setAttribute("stroke", state.grid ? "lightsteelblue" : "white");
+		}
 };
